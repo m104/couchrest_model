897c5b8fb3fa074a61af93b5e43e7f6c17176e2f
Adam Collins <adam.w.collins@gmail.com>
2011-04-25 00:34:46 -0700

    actually require the couchrest/database extension

--- lib/couchrest_model.rb
+++ lib/couchrest_model.rb
@@ -46,6 +46,7 @@ require "couchrest/model/designs/view"
 
 # Monkey patches applied to couchrest
 require "couchrest/model/support/couchrest"
+require "couchrest/database/casted_view"
 # Core Extensions
 require "couchrest/model/core_extensions/hash"
 require "couchrest/model/core_extensions/time_parsing"
